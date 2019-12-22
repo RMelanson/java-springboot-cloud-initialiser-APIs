8a6687602d01ca5a42b6e3ceb80f19e2e33500bb
Robin <robin.e.melanson@gmail.com>
2019-12-22 15:15:26 -0500

    WIP

--- src/main/java/springboot/ci/controllers/SystemCallController.java
+++ src/main/java/springboot/ci/controllers/SystemCallController.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import springboot.ci.services.InstallServices;
 import springboot.ci.services.SystemCallServices;
 
 @CrossOrigin("*")
